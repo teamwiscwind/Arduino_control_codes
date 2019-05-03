696f0523579a53f5fd3901b87db0fe0a94fdbcf4
teamwiscwind <49922597+teamwiscwind@users.noreply.github.com>
2019-05-03 18:24:52 -0500

    Update Arduino_Control.ino

--- Arduino_Control.ino
+++ Arduino_Control.ino
@@ -1,4 +1,3 @@
-
 #include "Arduino.h"
 
 // use digital pin8 as input pin for push button
@@ -11,34 +10,37 @@
 #define PULSE_SIG_ACT 9
 #define PULSE_SIG_DEACT 10
 
-#define PULSE_TIME 500
+#define PULSE_TIME 20
 
 static unsigned int state;
 int button;
 
 // the voltage at current sensing pin
-int v_current_sensing;
+float v_current_sensing;
 
 // the flag that the current is zero
 int zero_current;
 
-// set the threshold voltage at current sensing pin to be 0.004v (close to 0v)
-const int v_current_threshold = 0.004;
+// set the threshold voltage at current sensing pin to be 0.00v (close to 0v)
+const float v_current_threshold = 0.01;
 
 void setup() {
+  Serial.begin(9600);
   state = 1;
-  pinMode(BUTTON_PIN, INPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(PULSE_SIG_ACT, OUTPUT);
   pinMode(PULSE_SIG_DEACT, OUTPUT);
 }
 
 void loop() {
 
-  button = digitalRead(BUTTON_PIN);
+  button = !digitalRead(BUTTON_PIN);
   
   // check voltage of current sensing here and determine the boolean value of zero_current; 
   // based on Vout = RL/10K * I(Amps) on current sensor, RL = 10k, Vout = I in amps, I resolution is about 0.004888
   v_current_sensing = 5.0/1023.0*analogRead(CURRENT_SENSING);
+  Serial.println(v_current_sensing);
+
   if (v_current_sensing < v_current_threshold) {
     zero_current = 1;
   }
@@ -52,7 +54,7 @@ void loop() {
   switch (state)
   {
   case 1:
-    if(button == HIGH) {
+    if(button == LOW) {
       state = 2;
       
       // send pulse signal of breaking here, pulse has voltage 3.3v/5v
@@ -65,13 +67,14 @@ void loop() {
 
       // end the pulse
       digitalWrite(PULSE_SIG_ACT, LOW);
+      delay(100);
 
       break;
     }
     
     if(zero_current == 1) {
       state = 3;
-      
+      Serial.println("zero_current = 1");
       // send pulse signal of breaking here, pulse has voltage 3.3v/5v
 
       // start the pulse
@@ -82,6 +85,7 @@ void loop() {
 
       // end the pulse
       digitalWrite(PULSE_SIG_ACT, LOW);
+      delay(100);
 
       break;
     }
@@ -89,7 +93,7 @@ void loop() {
     break;
  
   case 2:
-    if(button == HIGH ) {
+    if(button == HIGH) {
       state = 1;
       
       // send pulse signal of breaking here, pulse has voltage 3.3v/5v
@@ -102,6 +106,7 @@ void loop() {
 
       // end the pulse
       digitalWrite(PULSE_SIG_DEACT, LOW);
+      delay(100);
 
     }
 
@@ -110,7 +115,7 @@ void loop() {
   case 3:
     if(zero_current == 0) {
       state = 1;
-      
+      Serial.println("zero_current = 0");
       // send pulse signal of breaking here, pulse has voltage 3.3v/5v
 
       // start the pulse
@@ -121,7 +126,7 @@ void loop() {
 
       // end the pulse
       digitalWrite(PULSE_SIG_DEACT, LOW);
-
+      delay(100);
     }
 
     break;
